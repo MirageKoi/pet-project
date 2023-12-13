368ab90a2ef8cedaf569d4f96bd39ca3f6676d3f
Denys <denismednov@gmail.com>
2023-12-13 11:54:18 +0200

    code refactoring

--- newstruct/services/users_service.py
+++ newstruct/services/users_service.py
@@ -7,39 +7,28 @@ from repositories.users_repository import UsersRepository
 class UsersService(object):
     users_repository: UsersRepository
 
-    # Get all users
-    async def all(self) -> dict:
+    async def all(self) -> list[User]:
         result = await self.users_repository.get_all_users()
-
         return result
 
-    # Get single user
     async def get(self, user_id: int):
         result = await self.users_repository.get_user(user_id=user_id)
-
         return result
 
-    # Create user
     async def create(self, new_user: UserCreateValidation) -> User:
         result = await self.users_repository.create_user(email=new_user.email, age=new_user.age)
         return result
 
-    # Update user
     async def update(self, user_id: int, **kwargs):
         # 1. Get user by id
         user = await self.users_repository.get_user(user_id)
-
         # Perform update on some fields
         user.email = kwargs.get("email", user.email)
         user.age = int(kwargs.get("age", user.age))
-
         # 3. Update in repository
-
         return await self.users_repository.update_user(user)
 
-    # Delete user
     async def delete(self, user_id: int):
         await self.users_repository.get_user(user_id=user_id)
         result = await self.users_repository.delete_user(user_id=user_id)
-
         return result
